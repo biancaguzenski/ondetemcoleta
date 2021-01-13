a79e69de29ab58ecbe13b5a26ba715888f644a93
Bianca <biancalway@gmail.com>
2021-01-13 09:48:42 -0300

    Set postgres as database.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_12_134540) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "collection_points", force: :cascade do |t|
     t.string "name"
     t.string "state"
