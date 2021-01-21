afae2cd5f4c9eea801eeefe597301bbf8e9f5e38
Bianca <biancalway@gmail.com>
2021-01-21 12:03:44 -0300

    Created unit tests for validations in model.

--- app/models/collection_point.rb
+++ app/models/collection_point.rb
@@ -1,8 +1,20 @@
 class CollectionPoint < ApplicationRecord
 
     validates :name, :state, :city, :address, presence: true
-    validates :name, format: { with: /\A[\p{L}\p{M}\-]+\z/, message: "Nome: Apenas letras, espaços e traços." }
-    validates :phone, :allow_blank => true, length: { within: 3..15, message: "Telefone válido: Entre 3 e 15 caracteres." }
+    
+    validates :name, format: { with: /\A[\p{L}\p{M}\-\s]+\z/, 
+                                     message: "Nome: Apenas letras, espaços e traços." }
+
+    validates :city, format: { with: /\A[\p{L}\p{M}\-\s]+\z/, 
+                                     message: "Cidade: Apenas letras, espaços e traços." }
+
+    validates :address, format: { with: /\A[\p{L}\p{M}\-\s\,0-9]+\z/, 
+                                        message: "Endereço: Apenas letras, espaços e vírgulas." }
+                                        
+    validates :phone, :allow_blank => true, 
+                      length: { within: 3..15, message: "Telefone válido: Entre 3 e 15 caracteres." }, 
+                      format: { with: /\A[\d\-()]+\z/, 
+                                message: "Telefone: Apenas números." }
     
     before_save :normalize_fields
 
--- test/models/collection_point_test.rb
+++ test/models/collection_point_test.rb
@@ -1,32 +1,65 @@
 require 'test_helper'
 
 class CollectionPointTest < ActiveSupport::TestCase
+
+  # tests for validation
+
   test "should not create new local without name" do
     local = CollectionPoint.new
     local.state = "State"
     local.city = "City"
     local.address = "Address"
-    assert_not local.save, "Tried to save the project without title"
+    assert_not local.save, "Tried to save local without name."
   end
   test "should not create new local without state" do
     local = CollectionPoint.new
     local.name = "Name"
     local.city = "City"
     local.address = "Address"
-    assert_not local.save, "Tried to save the project without state"
+    assert_not local.save, "Tried to save local without state."
   end
   test "should not create new local without city" do
     local = CollectionPoint.new
     local.name = "Name"
     local.state = "State"
     local.address = "Address"
-    assert_not local.save, "Tried to save the project without city"
+    assert_not local.save, "Tried to save local without city."
   end
   test "should not create new local without address" do
     local = CollectionPoint.new
     local.name = "Name"
     local.state = "State"
     local.city = "City"
-    assert_not local.save, "Tried to save the project without address"
+    assert_not local.save, "Tried to save local without address."
+  end
+  test "should not create new local with invalid characters in name" do
+    local = CollectionPoint.new
+    local.name = "Name["
+    assert_not local.save, "Tried to save local with invalid characteres in name."
+  end
+  test "should not create new local with invalid characters in city" do
+    local = CollectionPoint.new
+    local.city = "City["
+    assert_not local.save, "Tried to save local with invalid characteres in city."
+  end
+  test "should not create new local with invalid characters in address" do
+    local = CollectionPoint.new
+    local.address = "Address, 10["
+    assert_not local.save, "Tried to save local with invalid characteres in address"
+  end
+  test "should not create new local with invalid characters in phone" do
+    local = CollectionPoint.new
+    local.phone = "xxxxx"
+    assert_not local.save, "Tried to save local with invalid characteres in address"
+  end
+  test "should not create new local with invalid phone size - bigger" do
+    local = CollectionPoint.new
+    local.phone = "1234567890123456"
+    assert_not local.save, "Tried to save local with invalid phone size"
+  end
+  test "should not create new local with invalid phone size - smaller" do
+    local = CollectionPoint.new
+    local.phone = "12"
+    assert_not local.save, "Tried to save local with invalid phone size"
   end
 end
