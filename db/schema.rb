07405a2d581a8453a46e7608fa14a50c00ab1d40
Bianca <biancalway@gmail.com>
2021-01-27 10:22:09 -0300

    Testing reverting migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_193012) do
+ActiveRecord::Schema.define(version: 2021_01_21_140703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2021_01_26_193012) do
     t.string "obs"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "type_of_material"
   end
 
 end
